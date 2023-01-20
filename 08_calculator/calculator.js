b7ac2b34a57ef5586d69909c5584cc530a844aff
Cup-of-Coffee <larry.le124@gmail.com>
2023-01-20 18:05:42 -0500

    Solve problems 7-9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let num = 0;
+
+  for(i = 0; i < array.length; i++){
+    num += array[i]
+  }
+
+  if(array === ''){
+    return 0;
+  }
+
+  return num;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	let num = 1;
 
+  for(i = 0; i < array.length; i++){
+    num = array[i] * num;
+  }
+
+  if(array === ''){
+    return 0;
+  }
+
+  return num;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let result = 1;
+
+  for(i = 0; i < num2; i++){
+    result = result * num1;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  let decrNum = num;
+
+  for(i = 0; i < num; i++){
+    result = result * decrNum;
+    decrNum--;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (word) {  
+    const flipped = word.toLowerCase().replace(/[^a-z]/g,"");
 
+    return (flipped.split("").reverse().join("")) == flipped;
 };
 
 // Do not edit below this line
