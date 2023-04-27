01dfcbcdd2ed95399f227a344a25d7a7cceca022
Cup-of-Coffee <larry.le124@gmail.com>
2023-04-27 01:39:32 -0400

    Solve Palindromes and Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let newerNum = 0;
+    let olderNum = 1;
+    let tempNum;
+    let finalNum;
+
+    if(num === 0){
+        return 0;
+    };
+
+    if(num < 0){
+        return "OOPS"
+    }
+
+    for(i = 1; i < num; i++){
+        tempNum = olderNum;
+
+        olderNum = newerNum + olderNum;
+
+        newerNum = tempNum;
+    };
+
+    return finalNum = olderNum;
 };
 
 // Do not edit below this line
