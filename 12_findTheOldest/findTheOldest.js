b47f5772c8fc89077344f4a16e3d441804b4e3ac
Cup-of-Coffee <larry.le124@gmail.com>
2023-04-28 18:55:48 -0400

    Working on Problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    let peopleArray = array.map(yearOfBirth => yearOfBirth)
+    let oldestAge = 0;
+    let oldestPerson = "";
+    let actualAge;
+    let currentYear = new Date().getFullYear;
+
+    for(i = 0; i < array.length; i++){
+        let birthYear = array[i][yearOfBirth];
+        if(!(array[i][yearOfDeath])){
+            actualAge = currentYear - birthYear;
+        }else{
+            actualAge = yearOfDeath - birthYear;
+        }
+
+        if(oldestAge < actualAge){
+            oldestAge = actualAge;
+            oldestPerson = array[i][name]
+        }
+    }
+
+    return highestNum;
 };
 
 // Do not edit below this line
